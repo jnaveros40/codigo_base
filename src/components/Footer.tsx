84cfa1afa319f118719ca1daecf54f450299ba2b
jnaveros40 <jnaveros40@gmail.com>
2025-10-29 14:10:29 -0500

    fix: increase WhatsApp button icon size for better visibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -68,8 +68,8 @@ const Footer = () => {
               title="Contactar por WhatsApp"
             >
               <svg
-                width="20"
-                height="20"
+                width="28"
+                height="28"
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
